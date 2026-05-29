43a2450f0d7da4a87987a69360b46479dae5451a
ConniConni <nmtgktmo@gmail.com>
2026-05-29 13:40:44 +0900

    データ挿入処理実装 #2

--- roomfinder/src/db_lader.py
+++ roomfinder/src/db_lader.py
@@ -1,6 +1,7 @@
 import os
 
 import psycopg2
+from psycopg2 import sql
 from dotenv import load_dotenv
 
 
@@ -28,6 +29,24 @@ def db_connect(**config):
     return psycopg2.connect(**config)
 
 
+def execute_insert_query(cur, table, query_body, params=None):
+    """
+    引数で受け取ったsqlを実行する
+    Args:
+        cur: カーソル
+        table: データを挿入するテーブル名
+        sql: 実行するsql文
+        params: sqlに埋め込むパラメータ デフォルトはNone
+    """
+
+    query = sql.SQL("TRUNCATE TABLE {} CASCADE;").format(sql.Identifier(table))
+    cur.execute(query)
+    print(f"EXECUTE SQL: {cur.mogrify(query).decode('utf-8')}")
+
+    cur.execute(query_body, params)
+    print(f"EXECUTE SQL: {cur.mogrify(query_body, params).decode('utf-8')}")
+
+
 if __name__ == "__main__":
     conn = None
     db_config = get_db_config_property()
@@ -38,7 +57,8 @@ if __name__ == "__main__":
                 # データ読み込み
                 # データクレンジング
                 # INSERT文組み立て
-                # INSERT文実行
+                query = "INSERT INTO railway_stations (name, line_name, geom) VALUES ('天神', '貫線', ST_GeomFromText('POINT(130.39863 33.59126)', 4326));"
+                execute_insert_query(cur, "railway_stations", query)
                 print("○件のデータを登録しました。")
 
     except psycopg2.OperationalError as e:
