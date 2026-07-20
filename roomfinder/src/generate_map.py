fea46e6eea16f4a51604b396c6534564e77cc6b7
ConniConni <nmtgktmo@gmail.com>
2026-07-20 15:02:51 +0900

    複数のレイヤーを追加

--- roomfinder/src/generate_map.py
+++ roomfinder/src/generate_map.py
@@ -7,7 +7,7 @@ file_name = "map_output.html"
 def visualize_on_map_match_properties(point):
     target_lng = point[0]
     target_lat = point[1]
-    m = folium.Map(location=[target_lat, target_lng], zoom_start=15, show=True)
+    m = folium.Map(location=[target_lat, target_lng], zoom_start=15)
 
     # 中心点にマーカーを追加
     folium.Marker(
@@ -16,6 +16,25 @@ def visualize_on_map_match_properties(point):
         tooltip="クリックで詳細表示",
     ).add_to(m)
 
+    # 複数のレイヤーを変更可能にする
+    folium.TileLayer("CartoDB positron", name="CartoDB positron", show=False).add_to(m)
+    folium.TileLayer(
+        tiles="https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
+        attr="国土地理院",
+        name="国土地理院 航空写真",
+        show=False,
+    ).add_to(m)
+
+    folium.TileLayer(
+        tiles="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
+        attr="国土地理院",
+        name="国土地理院 淡色地図",
+        show=False,
+    ).add_to(m)
+
+    folium.LayerControl().add_to(m)
+
+    # HTMLファイルを出力
     m.save(file_name)
 
 
