335c157d13b72f59a0b26d68286ef97a11768626
ConniConni <nmtgktmo@gmail.com>
2026-07-25 18:02:03 +0900

    各定数・変数を追加 #28

--- review_tr_postgis/search_nearby.py
+++ review_tr_postgis/search_nearby.py
@@ -2,9 +2,26 @@ import logging
 from pathlib import Path
 from datetime import datetime
 
+# --- 定数 ---
+TARGET_POINTS = [
+    (35.92, 139.48, "川越A"),
+    (34.68, 135.99, "奈良B"),
+    (34.68, 135.99, "奈良C"),
+    (35.41, 136.76, "岐阜D"),
+    (43.30, 142.42, "岐阜E"),
+    (33.32, 130.94, "日田F"),
+]
+SEARCH_RADIUS_M = 1000
+MAX_SEARCH_RADIUS_M = 10000
+MAX_WORKERS = 3
+
 # --- 変数 ---
 # ログファイル格納先
 log_dir = Path(__file__).parent / "logs"
+# 単位:度でのざっくりとした絞り込み用の係数
+search_radius_padding_factor = SEARCH_RADIUS_M / 111000 * 1.5
+# 取得結果出力先パス
+output_dir = Path(__file__).parent / "result" / "search_results.csv"
 
 
 # --- 関数 ---
@@ -45,6 +62,17 @@ def setup_logger(path, level=logging.DEBUG):
     # ルートロガーにハンドラーを追加
     root_logger.addHandler(console_handler)
     root_logger.addHandler(file_handler)
+
+
+def validate_params():
+    """
+    データ抽出に使用する定数及び変数がツールが許容する値であることを確認する
+
+    Args:
+        なし
+    return:
+        なし
+    """
 
 
 if __name__ == "__main__":
