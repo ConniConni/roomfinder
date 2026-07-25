a30972756c006f82727f39d50cc4880d184914ea
ConniConni <nmtgktmo@gmail.com>
2026-07-25 14:21:19 +0900

    feat: ロガー取得関数の実装  #24

---
+++ review_tr_postgis/search_nearby.py
@@ -0,0 +1,45 @@
+import logging
+from pathlib import Path
+from datetime import datetime
+
+# --- 変数 ---
+# ログファイル格納先
+log_dir = Path(__file__).parent / "logs"
+
+
+# --- 関数 ---
+def setup_logger(path, level=logging.DEBUG):
+    """
+    ロガーの設定を行う
+
+    Args:
+        path (str | Path): ログファイルを格納するディレクトリパス
+        level (str): ログレベル
+    """
+    # ログファイルのパスを整形
+    now_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = path / f"debug_{now_str}.log"
+
+    # ルートロガーを取得し、ログレベルを設定
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
+    # コンソールハンドラーを作成: ログレベルINFO
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # ファイルハンドラーを作成: ログレベルDEBUG
+    file_handler = logging.FileHandler(file_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # フォーマッタを作成しハンドラーに追加
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(threadName)s - %(lineno)d - %(message)s"
+    )
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # ルートロガーにハンドラーを追加
+    root_logger.addHandler(console_handler)
+    root_logger.addHandler(file_handler)
