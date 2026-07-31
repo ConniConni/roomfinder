691eaca9ebbaee93773a24fd5abf907c07b1ca85
ConniConni <nmtgktmo@gmail.com>
2026-07-31 15:44:55 +0900

    クエリパラメータ

--- review_tr_postgis/fast_api/main.py
+++ review_tr_postgis/fast_api/main.py
@@ -8,9 +8,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-# httpメソッドで"/"にgetでアクセスがあったら処理を行う
-@app.get(
-    "/countries/{country_name}"
-)  # "/":パスオペレーション パスパラメータで動的な変数を組み込める
-async def country(country_name: str):
-    return {"country_name": country_name}
+# # httpメソッドで"/"にgetでアクセスがあったら処理を行う
+# @app.get(
+#     "/countries/{country_name}"
+# )  # "/":パスオペレーション パスパラメータで動的な変数を組み込める
+# async def country(country_name: str):
+#     return {"country_name": country_name}
+
+
+# /countries/?country_name=america&country_no=2でアクセスするとクエリパラメータの内容を表示する
+@app.get("/countries/")  # パスパラメータにない引数を使うとクエリパラメータとなる
+async def country(country_name: str = "japan", country_no: int = 1):
+    return {"country_name": country_name, "country_no": country_no}
