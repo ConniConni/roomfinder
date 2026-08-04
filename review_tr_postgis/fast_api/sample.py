65942934101b2f8ce833ff9b67dd3b805e32fe78
ConniConni <nmtgktmo@gmail.com>
2026-08-04 09:55:39 +0900

    httpメソッドpostの確認

--- review_tr_postgis/fast_api/sample.py
+++ review_tr_postgis/fast_api/sample.py
@@ -3,3 +3,7 @@ import requests
 r = requests.get(url="http://127.0.0.1:8000/")
 result = r.json()
 print(result)
+
+r_post = requests.post(url="http://127.0.0.1:8000/", json={"x": 3, "y": 4})
+result_post = r_post.json()
+print(result_post)
