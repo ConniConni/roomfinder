134611560156cb1b4fb4766f873d24445da3daf6
ConniConni <nmtgktmo@gmail.com>
2026-08-04 10:17:25 +0900

    mian関数化

--- review_tr_postgis/fast_api/sample.py
+++ review_tr_postgis/fast_api/sample.py
@@ -7,3 +7,19 @@ print(result)
 r_post = requests.post(url="http://127.0.0.1:8000/", json={"x": 3, "y": 4})
 result_post = r_post.json()
 print(result_post)
+
+
+def main():
+    url = "http://127.0.0.1:8000/"
+    body = {"x": 3, "y": 4}
+
+    res_get = requests.get(url)
+
+    print(res_get.json())
+
+    res_post = requests.post(url, json=body)
+    print(res_post.json())
+
+
+if __name__ == "__main__":
+    main()
