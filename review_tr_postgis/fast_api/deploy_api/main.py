2cc1d67858bbd0d2503b7263c07be694e159b5d8
ConniConni <nmtgktmo@gmail.com>
2026-08-04 14:23:35 +0900

    DB接続確認

--- review_tr_postgis/fast_api/deploy_api/main.py
+++ review_tr_postgis/fast_api/deploy_api/main.py
@@ -1,5 +1,12 @@
+import os
+import psycopg2
+from pathlib import Path
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+env_dir = Path(__file__).parent.parent.parent.parent / ".env"
 
 
 class Data(BaseModel):
@@ -7,12 +14,43 @@ class Data(BaseModel):
     y: float
 
 
+def db_config(env_path):
+    load_dotenv(env_path)
+
+    config = {
+        "host": os.getenv("POSTGRES_HOST", "localhost"),
+        "port": os.getenv("POSTGRES_PORT"),
+        "database": os.getenv("POSTGRES_DB"),
+        "user": os.getenv("POSTGRES_USER"),
+        "password": os.getenv("POSTGRES_PASSWORD"),
+    }
+
+    return config
+
+
 app = FastAPI()
 
 
 @app.get("/")
 async def index():
+
     return {"message": "Hello Deta!"}
+
+
+@app.get("/result")
+async def get_search_info():
+    try:
+        config = db_config(env_dir)
+        with psycopg2.connect(**config) as conn:
+            pass
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT 1, 'test', 'bakery', ST_AsText(ST_GeomFromText('POINT(139.48 35.92)', 4326));"
+                )
+                rows = cur.fetchall()
+        return {"result": rows[0]}
+    except Exception as e:
+        return {"status": "error", "error_type": str(type(e)), "message": str(e)}
 
 
 @app.post("/")
