b4941cd86414b506f5bccc141d16c8373e5d2d55
ConniConni <nmtgktmo@gmail.com>
2026-08-06 11:08:01 +0900

    環境変数未設定時の処理を修正

--- review_tr_postgis/fast_api/deploy_api/main.py
+++ review_tr_postgis/fast_api/deploy_api/main.py
@@ -261,7 +261,10 @@ def get_db_config(env_path):
     log_message = f"【接続エラー】以下の環境変数が未設定です。{', '.join(missing_keys)}"
     if missing_keys:
         logger.error(log_message)
-        return log_message
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=log_message,
+        )
 
     return config
 
